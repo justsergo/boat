bc2e22e71e0d8af66a6517d36db2b2e69a3be2ed
sdan <sergeydanilov2019@gmail.com>
2022-12-21 15:46:38 +0300

    method for moving boat

--- src/javascript/canvas.js
+++ src/javascript/canvas.js
@@ -1,36 +1,82 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let x = canvas.width/2;
-let y = canvas.height-30;
-const ballRadius = 10;
-let dx = 2;
-let dy = -2;
+const boatWidth = 43;
+const boatHeight = 118;
+const img = new Image();
 
-const drawBall = () => {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
+const saveX = canvas.width-boatWidth;
+const saveY = canvas.height-boatHeight / 2;
+
+let boatX = (saveX)/2;
+let boatY = (saveX)/2;
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+
+img.onload = () => {   
+    ctx.translate(boatWidth/2,boatHeight/2);
+    ctx.drawImage(img,41,5,boatWidth,boatHeight,-boatWidth/2,-boatHeight/2,boatWidth,boatHeight); 
+    console.log(ctx.getTransform());
+};
+    
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {    
+    if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+    if(e.keyCode == 38) {
+        upPressed = true;
+    }
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    if(e.keyCode == 40) {
+        downPressed = true;
+    }   
+    console.log(ctx.getTransform()); 
+};
+
+function keyUpHandler(e) {    
+    if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+    if(e.keyCode == 38) {
+        upPressed = false;
+    }
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    if(e.keyCode == 40) {
+        downPressed = false;
+    }
+};
 
 const draw = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
+    ctx.clearRect(-boatWidth, -boatHeight, canvas.width, canvas.height);
+    if(rightPressed) {
+        ctx.rotate((Math.PI/180)*5)
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
+    if(leftPressed) {
+        ctx.rotate(-(Math.PI/180)*5)
+    }     
+    if(downPressed) {
+        const newY = boatY + 7;
+        boatY =  newY < saveX ? newY : saveX;
+        ctx.translate(0,-10)
     }
-    
-    x += dx;
-    y += dy;
-window.requestAnimationFrame(draw)
+    if(upPressed) {
+        const { f } = ctx.getTransform();
+        if(f + 7 < saveY) ctx.translate(0,7)
+    }
+  ctx.drawImage(img,41,5,boatWidth,boatHeight,-boatWidth/2,-boatHeight/2,boatWidth,boatHeight);
+    window.requestAnimationFrame(draw);
+};   
 
-}
+window.requestAnimationFrame(draw);
 
-// setInterval(draw, 10);
-window.requestAnimationFrame(draw)
+img.src = 'src/assets/PNG/Boats_color1/Boat_color1_4.png';
