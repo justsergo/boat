3aa7be84081107dee4d9aeb53c03091d18615c4d
sdan <sergeydanilov2019@gmail.com>
2022-12-22 18:20:32 +0300

    new class

--- src/javascript/boat.js
+++ src/javascript/boat.js
@@ -1,8 +1,21 @@
 class Boat {
-    constructor(height, width) {
+    constructor(width,height) {
         this.height = height;
         this.width = width;
+        this.x = width/2;
+        this.y = height/2;
+        this.step = 7;
       }
+
+    moveVertical(isUp) {
+      this.y += isUp ? this.step : -this.step;
+    }
+
+    moveHorizontal(isRight) {
+      this.x += isRight ? this.step : -this.step;
+    }
+
+
 }
 
 export default Boat
--- src/javascript/canvas.js
+++ src/javascript/canvas.js
@@ -1,14 +1,43 @@
+class Boat {
+    constructor(width,height) {
+        this.height = height;
+        this.width = width;
+        this.x = width/2;
+        this.y = height/2;
+        this.step = 7;
+      }
+
+    moveVertical(isUp) {
+      this.y += isUp ? this.step : -this.step;
+    }
+
+    moveHorizontal(isRight) {
+      this.x += isRight ? this.step : -this.step;
+    }
+
+
+}
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
 const boatWidth = 43;
 const boatHeight = 118;
 const img = new Image();
+img.onload = () => {   
+    window.requestAnimationFrame(draw);
+};
+img.src = 'src/assets/PNG/Boats_color1/Boat_color1_4.png';
 
+
+
+const boat = new Boat(canvas.width, canvas.height);
+console.log(boat)
 const saveX = canvas.width-boatWidth;
 const saveY = canvas.height-boatHeight / 2;
-const moveBoat = 7;
+const stepBoat = 7;
 const rotateBoat = (Math.PI/180)*1;
+const maxWidth = canvas.width;
+const maxHeight = canvas.height;
 
 let boatX = (saveX)/2;
 let boatY = (saveX)/2;
@@ -16,18 +45,7 @@ let rightPressed = false;
 let leftPressed = false;
 let upPressed = false;
 let downPressed = false;
-
-const inRange = (number,min,max)=> {
-    return number>=min && number<=max;
-}
-
-
-img.onload = () => {   
-    ctx.translate(boatWidth/2,boatHeight/2);
-    ctx.drawImage(img,41,5,boatWidth,boatHeight,-boatWidth/2,-boatHeight/2,boatWidth,boatHeight); 
-    console.log(ctx.getTransform());
-};
-    
+ 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -64,30 +82,38 @@ function keyUpHandler(e) {
 
 const draw = () => {
     ctx.clearRect(-boatWidth, -boatHeight, canvas.width, canvas.height);
-    if(rightPressed) {
-        ctx.rotate(rotateBoat)
-    }
-    if(leftPressed) {
-        ctx.rotate(-rotateBoat)
-    }     
+    // let moveBoat = stepBoat;
+    // if(rightPressed) {
+    //     ctx.rotate(rotateBoat)
+    // }
+    // if(leftPressed) {
+    //     ctx.rotate(-rotateBoat)
+    // }     
     if(downPressed) {
-        const { f:y, b:rx, c:ry, e:x } = ctx.getTransform();
-        const newX = x - rx*moveBoat ;
-        const newY = Math.abs(y -(ry*ry+rx*rx) -7);
-        console.log('down',newY)
-        if( newY <= canvas.height && newY > 0 ) ctx.translate(0,-moveBoat)
+        boat.moveVertical()
+        // const { f:y, b:rx, c:ry, e:x } = ctx.getTransform();
+        // const newX = x - rx*stepBoat ;
+        // const newY = Math.abs(y -(ry*ry+rx*rx) -7);
+        // console.log('down',newY)
+        // if(newY + stepBoat > maxHeight) moveBoat = maxHeight - y;
+        // if(newY + stepBoat < 0) moveBoat = y;        
+
+        // ctx.translate(0,-moveBoat)
     }
     if(upPressed) {
-        const { f:y, b:rx, c:ry, e:x } = ctx.getTransform();
-        const newX = x +rx*moveBoat ;
-        const newY =  Math.abs(y +ry*ry+rx*rx+7);
-        console.log('up',newY)
-        if(newY <= canvas.height && newY > 0  ) ctx.translate(0,moveBoat)
+        boat.moveVertical(true)
+        // const { f:y, b:rx, c:ry, e:x } = ctx.getTransform();
+        // const newX = x +rx*stepBoat ;
+        // const newY =  Math.abs(y +ry*ry+rx*rx+7);
+        // console.log('up',newY)
+
+        // if(newY + stepBoat > maxHeight) moveBoat = maxHeight - y;
+        // if(newY + stepBoat < 0) moveBoat = y;
+        
+        // ctx.translate(0,moveBoat)
     }
-  ctx.drawImage(img,41,5,boatWidth,boatHeight,-boatWidth/2,-boatHeight/2,boatWidth,boatHeight);
+    ctx.drawImage(img,boat.x,boat.y,128,128);
     window.requestAnimationFrame(draw);
 };   
 
-window.requestAnimationFrame(draw);
 
-img.src = 'src/assets/PNG/Boats_color1/Boat_color1_4.png';
